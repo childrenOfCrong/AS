ef2c3199a91561b4c0c4c52e0bd8c3451ff22310
Head <mrdoo309@gmail.com>
2018-11-14 22:08:44 +0900

    add class LinkedList, Node

--- HTMLhead/linkedList.js
+++ HTMLhead/linkedList.js
@@ -1,44 +1,14 @@
-class Node {
-    constructor(data) {
-        this.data = data
-        this.next = null 
-    }
-}
-
 class LinkedList {
     constructor() {
         this.length = 0
         this.head = null
     }
-    
-    add(value) {
-        var node = new Node(value)
-        if(this.head === null) {
-            this.head = node
-            this.length++
-            return;
-        }
-        while(this.head.next !== null) {
-            this.head = this.head.next
-        }
-        this.head.next = node
-        this.length++
-        return node
-    }
-
-    search(position) {
-        
-    }
-
-    remove() {
-        
-    }
 }
 
-const linkedList = new LinkedList()
-console.log(linkedList)
-linkedList.add(3)
-linkedList.add(4)
-linkedList.add(5)
-console.log(linkedList)
-
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+    
