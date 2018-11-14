840c69524c12813daff466cd69863648c50b2ea8
Head <mrdoo309@gmail.com>
2018-11-14 22:22:52 +0900

    linkedlist javascript file finish

--- HTMLhead/linkedList.js
+++ HTMLhead/linkedList.js
@@ -11,4 +11,36 @@ class Node {
         this.next = null
     }
 }
-    
+
+class List {
+    constructor(linkedList) {
+        this.linkedList = linkedList
+    }
+
+    add(value) {//앞에부터 더하는 방식
+        let node = new Node(value)
+        let current = this.linkedList.head
+        if (current === null) {
+            this.linkedList.head = node
+            this.linkedList.length++
+            return
+        }
+        while (current.next) {
+            current = current.next
+        }
+        current.next = node
+        this.linkedList.length++
+        return node
+    }
+
+    remove() {//앞의값만 지우는 방식
+        var remove = this.linkedList.head;
+        this.linkedList.head = this.linkedList.head.next; // head를 두 번째 노드로 교체
+        this.linkedList.length--;
+        return remove;
+    }
+}
+
+const linkedList = new LinkedList()
+const list = new List(linkedList)
+
