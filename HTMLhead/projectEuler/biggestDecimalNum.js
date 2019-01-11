380d283b4aff967a602a2ede01daa8ed88e0035c
Head <mrdoo309@gmail.com>
2019-01-11 11:26:22 +0900

    line check

--- HTMLhead/projectEuler/biggestDecimalNum.js
+++ HTMLhead/projectEuler/biggestDecimalNum.js
@@ -1,12 +1,12 @@
 function solution(number) {
-    for(let i =2; i < number; i++) {
-        if(number % i === 0) {
-            number = number / i
-            i = 2
-            continue;
-        }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      number = number / i;
+      i = 2;
+      continue;
     }
-    return number;
+  }
+  return number;
 }
 
-console.log(solution(600851475143))
+console.log(solution(600851475143));
