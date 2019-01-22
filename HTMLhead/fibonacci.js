c02a95fcd5d461a87de64d4c0e55531c19b85420
Head <mrdoo309@gmail.com>
2019-01-22 13:39:51 +0900

    make fibonacci

---
+++ HTMLhead/fibonacci.js
@@ -0,0 +1,27 @@
+const makeFibonaccci = function (max, result = 0, pre1 = 0, pre2 = 1) {
+  function sum() {
+    result = pre1 + pre2
+    pre1 = pre2
+    pre2 = result;
+    return (result > max)? null: result;
+  }
+  return {
+    next: () => {
+      return {
+        value: sum(),
+        done: (result > max)? true: false
+      }
+    }
+  };
+}
+
+const fun = makeFibonaccci(8);
+// console.log(fun());
+// console.log(fun());
+// console.log(fun());
+// console.log(fun());
+// console.log(fun());
+for(let i = 0; i < 10; i++) {
+  console.log(fun.next());
+}
+
