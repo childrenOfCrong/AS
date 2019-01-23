96084c2ca558b8384635e76bcdc5a2ae9913d62c
Head <mrdoo309@gmail.com>
2019-01-23 15:38:40 +0900

    에라토스테네스의 채 구현 실패..

--- HTMLhead/projectEuler/sumOfPrime.js
+++ HTMLhead/projectEuler/sumOfPrime.js
@@ -1,30 +1,19 @@
-function solution(n) {
-  const arr = new Array(n).fill(0).map((v, i) => i)
+function solution(num) {
+  var arr = new Array(num).fill(0).map((v, i) => i)
+  let answerArr = [];
   arr.forEach(v => {
-    if(v % 2 === 0) {
-      arr[v] = 0
-    }
+    if(v % 2 === 0) return;
+    answerArr.push(v)
   })
+  arr = answerArr
   arr.forEach(v => {
-    if(v % 3 === 0) {
-      arr[v] = 0
-    }
-  })
-  let answer = 0
-  arr.forEach((v, i) => {
-    if(v === 0 || v === 1) {
-      return;
-    }
-
-    for(let j = 2; j < v; j++) {
-      if(v % j === 0) {
-        return;
+    if(v === 1 || v === 0) return;
+    for(let i = 2; i < v; i++) {
+      if(v % i === 0) {
+        
       }
     }
-    answer += v
   })
-  return answer+2;
 }
-//에라토스테네스의 체 사용요함
-console.log(solution(2000000));
 
+solution(2000000);
