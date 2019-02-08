8740972bdc45699e6a3af8788c3bfd6f2ebbd1ca
HTMLhead <mrdoo309@gmail.com>
2019-02-08 23:15:22 +0900

    add jumping on the cloud

--- HTMLhead/hackerrank/JumpingOnTheCloud.js
+++ HTMLhead/hackerrank/JumpingOnTheCloud.js
@@ -2,24 +2,20 @@ function solution(c) {
   let count = 0
   for (let i = 0; i < c.length; i++) {
     debugger;
-    if(c[i] === 0) {
-      if(c[i+2] === 1) {
-        count++
-        continue;
-      }
-      if(c[i+1] === 1) {
-        count++
-        i++
-        continue;
-      }
-      if(c[i+2] === 0) {
-        count++
-        i++
-        continue;
-      }
+    if(c[i+2] === 1) {
+      count++
+    } else if (c[i+1] === 1) {
+      count++
+      i++
+    } else if (c[i+1] === 0 && c[i+2] === 0) {
+      count++
+      i++
+    } else if (c[i+1] === 0) {
+      count++
     }
   }
   return count;
 }
+//그냥 모든 경우 때려넣기...
 
-console.log(solution([ 0, 0, 1, 0, 0, 1, 0 ]));
+console.log(solution([0, 0 ,0, 1 ,0 ,0]));
