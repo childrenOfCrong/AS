8815ee3dbe15edfbbf7832627b27f4f083149123
HTMLhead <mrdoo309@gmail.com>
2019-03-20 14:23:20 +0900

    Chore: numberExpress.js 푸는중

---
+++ HTMLhead/hackerrank/newYearChaos.js
@@ -0,0 +1,6 @@
+function solution(p) {
+  
+}
+
+console.log(solution([2,3,4,7,6,5]))
+
---
+++ HTMLhead/programmers/numberExpress.js
@@ -0,0 +1,8 @@
+function solution(n) {
+  var arr = new Array(n).fill(0).map((v, i) => i)
+  arr.forEach(v => {
+    v
+  })
+}
+
+solution(15)
