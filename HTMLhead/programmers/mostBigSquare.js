b5d4ba36a8c613a77a8d62bfbd40c07d3b779388
HTMLhead <mrdoo309@gmail.com>
2019-04-08 12:01:38 +0900

    Solve: 효율성 실패 mostBigSquare.js

--- HTMLhead/programmers/mostBigSquare.js
+++ HTMLhead/programmers/mostBigSquare.js
@@ -1,28 +1,30 @@
 function solution(board) {
-    let wLength = 0
-    let widthLength = [];
-    for (let i = 0; i < board[0].length; i++) {
-        for (let j = 0; j < board.length; j++) {
-            if (board[i][j] === board[i][j + 1] && board[i][j] === 1) {
-                wLength = wLength + 1
-            } else if (board[i][j] === 0) {
-                widthLength.push(wLength);
-                let wLength = 0
-            }
+  let Xlength = board[0].length;
+  let Ylength = board.length;
+	let answer = 0;
+
+  if (Xlength < 2 || Ylength < 2) {
+    for (let i = 0; i < Xlength; i++) {
+      for (let j = 0; j < Ylength; j++) {
+        if (board[i][j] === 1) {
+          return 1;
         }
+      }
     }
-    let hLength = 0
-    let heightLength = [];
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            if (board[i][j] === board[i][j + 1] && board[i][j] === 1) {
-                hLength + hLength + 1
-            } else if (board[i][j] === 0) {
-                heightLength.push(hLength)
-                let hLength = 0
-            }
+  } else {
+    for (let i = 1; i < Ylength; i++) {
+      for (let j = 1; j < Xlength; j++) {
+        if (board[i][j] === 1) {
+          board[i][j] =
+            Math.min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1]) + 1;
+          if (board[i][j] > answer) {
+            answer = board[i][j];
+          }
         }
+      }
     }
-    
-    
+  }
+  return answer * answer
 }
+
+console.log(solution([[0, 0, 1, 1], [1, 1, 1, 1]]));
